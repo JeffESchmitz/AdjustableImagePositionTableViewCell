4b4bc122dcb223230d3508d9ed00f6f049803a04
jschmitz <jeff.schmitz@realpage.com>
2016-06-06 05:33:28 -0500

    Revert "beetlejuice..."
    This reverts commit 0c797ab988810da72923d870c5f490a4309a0f0d.

--- AdjustableImagePositionTableViewCell/CustomCell.swift
+++ AdjustableImagePositionTableViewCell/CustomCell.swift
@@ -10,64 +10,14 @@ import UIKit
 
 class CustomCell: UITableViewCell {
 
-    var textField1: UITextField!
-    var textField2: UITextField!
-    var textField3: UITextField!
-    var textField4: UITextField!
-    
-    ///------------
-    //Method: Init with Style
-    //Purpose:
-    //Notes: This will NOT get called unless you call "registerClass, forCellReuseIdentifier" on your tableview
-    ///------------
-    override init(style: UITableViewCellStyle, reuseIdentifier: String!) {
-        //First Call Super
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        //Initialize Text Field
-        textField1 = UITextField(frame: CGRect(x: 0, y: 9, width: 44.00, height: 44.00));
-//        textField1 = UITextField(frame: CGRectZero)
-        textField1.backgroundColor = UIColor.redColor()
-
-        textField2 = UITextField(frame: CGRect(x: 80, y: 9, width: 44.00, height: 44.00));
-//        textField2 = UITextField(frame: CGRectZero)
-        textField2.backgroundColor = UIColor.orangeColor()
-
-        textField3 = UITextField(frame: CGRect(x: 160, y: 9, width: 44.00, height: 44.00));
-//        textField3 = UITextField(frame: CGRectZero)
-        textField3.backgroundColor = UIColor.purpleColor()
-
-        textField4 = UITextField(frame: CGRect(x: 240, y: 9, width: 44.00, height: 44.00));
-//        textField4 = UITextField(frame: CGRectZero)
-        textField4.backgroundColor = UIColor.blueColor()
-
-        //Add TextField to SubView
-        contentView.addSubview(textField1)
-        contentView.addSubview(textField2)
-        contentView.addSubview(textField3)
-        contentView.addSubview(textField4)
-    }
-    
-    ///------------
-    //Method: Init with Coder
-    //Purpose:
-    //Notes: This function is apparently required; gets called by default if you don't call "registerClass, forCellReuseIdentifier" on your tableview
-    ///------------
-    required init(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)!
-    }
-    
-    override func prepareForReuse() {
-        super.prepareForReuse()
-    }
+    @IBOutlet weak var imageView1: UIImageView!
+    @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var imageView3: UIImageView!
+    @IBOutlet weak var imageView4: UIImageView!
     
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        textField1.frame = CGRectMake(0, 9, 44.0, 44.0)
-        textField2.frame = CGRectMake(40, 9, 44.0, 44.0)
-        textField3.frame = CGRectMake(80, 9, 44.0, 44.0)
-        textField4.frame = CGRectMake(120, 9, 44.0, 44.0)
+        self.imageView2.frame = CGRectMake(0, 0, 70, 70)
     }
-    
 }
--- AdjustableImagePositionTableViewCell/ViewController.swift
+++ AdjustableImagePositionTableViewCell/ViewController.swift
@@ -12,36 +12,138 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     @IBOutlet weak var tableView: UITableView!
     
-    override func viewWillAppear(animated: Bool)
-    {
-        //First Call Super
-        super.viewWillAppear(animated)
-        
-        //Register the Custom DataCell
-        tableView.registerClass(CustomCell.classForCoder(), forCellReuseIdentifier: "CustomCell")
-    }
+    var tableData = ["Ferrari", "BMW", "Mitsubishi", "Lambo"]
+    // Tuple holds car name, logo image, hidden boolean
+    var tupleTableData: [(String, UIImage, Bool)]? = nil
     
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadSampleData()
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+//        return self.tableData.count
+        return 1
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell 	{
-        //Get Reference to Cell
-        let cell = self.tableView.dequeueReusableCellWithIdentifier("CustomCell") as! CustomCell
+        let cell = tableView.dequeueReusableCellWithIdentifier("CustomCell", forIndexPath: indexPath) as! CustomCell
         
-        //...Do Stuff
+        // basic image displaying
+//        cell.imageView1.image = UIImage(named: getRandomCarName())
+//        cell.imageView2.image = UIImage(named: getRandomCarName())
+//        cell.imageView3.image = UIImage(named: getRandomCarName())
+//        cell.imageView4.image = UIImage(named: getRandomCarName())
+//        
+//        for car in tupleTableData! {
+//            print(car)
+//        }
         
-        //Return Cell
+        // show matching image from the tuple index
+        cell.imageView1.image = UIImage(named: tupleTableData![0].0)
+        if tupleTableData![1].2 {
+            cell.imageView2.image = UIImage(named: tupleTableData![1].0)
+        }
+//        cell.imageView2.image = UIImage(named: tupleTableData![1].0)
+        cell.imageView3.image = UIImage(named: tupleTableData![2].0)
+        cell.imageView4.image = UIImage(named: tupleTableData![3].0)
+
+//        // 1st attempt at moving the 2nd imageview
+//        cell.imageView1.image = (tupleTableData![0].2) ? UIImage(named: tupleTableData![0].0) : nil
+//        cell.imageView2.image = (tupleTableData![1].2) ? UIImage(named: tupleTableData![1].0) : nil
+//        cell.imageView3.image = (tupleTableData![2].2) ? UIImage(named: tupleTableData![2].0) : nil
+//        cell.imageView4.image = (tupleTableData![3].2) ? UIImage(named: tupleTableData![3].0) : nil
+//        
+//        let imageView2OriginX = cell.imageView2.frame.origin.x
+//        
+//        // move imageView2 to zero position (over imageView1) and compare Origin and MovedX positions
+////        cell.imageView2.frame = CGRect(x: 0, y: cell.imageView2.frame.origin.y, width: cell.imageView2.frame.width, height: cell.imageView2.frame.height)
+////        cell.imageView2.frame = CGRect(origin: cell.imageView1.frame.origin, size: cell.imageView1.frame.size)
+////        cell.imageView1.hidden = true
+//        let imageView2MovedX = cell.imageView2.frame.origin.x
+//        
+//        if imageView2OriginX == imageView2MovedX {
+//            print("ImageView2 did not move its X position!")
+//        }
+        
         return cell
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         print("Row \(indexPath.row) selected")
     }
+    
+    private func getRandomCarName() -> String {
+        let result = tableData[Int(arc4random() % 4)]
+        return result
+    }
+    
+    private func loadSampleData() {
+        loadAllCarData()
+//        loadFirstThirdFourthCarData()
+//        loadFirstSecondFourthCarData()
+//        loadSecondThirdFourthCarData()
+    }
 
+    private func loadAllCarData() {
+        
+        var carName = "Ferrari"
+        tupleTableData = [(carName, UIImage(named: carName)!, true)]
+        
+        carName = "BMW"
+        tupleTableData?.append((carName, UIImage(named: carName)!, true))
+        
+        carName = "Mitsubishi"
+        tupleTableData?.append((carName, UIImage(named: carName)!, true))
+        
+        carName = "Lambo"
+        tupleTableData?.append((carName, UIImage(named: carName)!, true))
+    }
+    
+    private func loadFirstThirdFourthCarData() {
+        
+        var carName = getRandomCarName()
+        tupleTableData = [(carName, UIImage(named: carName)!, true)]
+        
+        carName = getRandomCarName()
+        tupleTableData?.append((carName, UIImage(named: carName)!, false))
+        
+        carName = getRandomCarName()
+        tupleTableData?.append((carName, UIImage(named: carName)!, true))
+        
+        carName = getRandomCarName()
+        tupleTableData?.append((carName, UIImage(named: carName)!, true))
+    }
+    
+    private func loadFirstSecondFourthCarData() {
+        
+        var carName = getRandomCarName()
+        tupleTableData = [(carName, UIImage(named: carName)!, true)]
+        
+        carName = getRandomCarName()
+        tupleTableData?.append((carName, UIImage(named: carName)!, true))
+        
+        carName = getRandomCarName()
+        tupleTableData?.append((carName, UIImage(named: carName)!, false))
+        
+        carName = getRandomCarName()
+        tupleTableData?.append((carName, UIImage(named: carName)!, true))
+    }
+    
+    private func loadSecondThirdFourthCarData() {
+        
+        var carName = getRandomCarName()
+        tupleTableData = [(carName, UIImage(named: carName)!, false)]
+        
+        carName = getRandomCarName()
+        tupleTableData?.append((carName, UIImage(named: carName)!, true))
+        
+        carName = getRandomCarName()
+        tupleTableData?.append((carName, UIImage(named: carName)!, true))
+        
+        carName = getRandomCarName()
+        tupleTableData?.append((carName, UIImage(named: carName)!, true))
+    }
     
 }
